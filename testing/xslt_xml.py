17957522ca81780e36ba98a7172258ac80772698
Thomas Lawson <tnl495@bham.ac.uk>
2015-07-16 11:24:08 +0100

    git rm stuff

--- testing/xslt_xml.py
+++
@@ -1,7 +0,0 @@
-import lxml.etree as ET
-
-dom = ET.parse("small.pwiz.1.1.mzML.xml")
-xslt = ET.parse("mz2tom.xsl")
-transform = ET.XSLT(xslt)
-newdom = transform(dom)
-print(ET.tostring(newdom, pretty_print=True))

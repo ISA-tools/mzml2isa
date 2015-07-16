7d99d5fb99585ff7e15091bcce4da41b7f5663d8
Thomas Lawson <tnl495@bham.ac.uk>
2015-07-16 14:42:02 +0100

    git rm

--- scripts/examples.py
+++ scripts/examples.py
@@ -19,33 +19,33 @@ parsing.full_parse(in_dir, out_dir, "testing")
 #====================================================
 # Just get meta information from a SINGLE mzML file
 #====================================================
-# from mzml_2_isa import mzml
-# onefile = os.path.join(in_dir,"samp1.mzML")
-# mm = mzml.mzMLmeta(onefile)
-#
-# # Dictionary format
-# print mm.meta
-#
-# # JSON format
-# print mm.meta_json
-#
-# # Naming compatible with ISA-Tab
-# print mm.meta_isa
+from mzml_2_isa import mzml
+onefile = os.path.join(in_dir,"samp1.mzML")
+mm = mzml.mzMLmeta(onefile)
+
+# Dictionary format
+print mm.meta
+
+# JSON format
+print mm.meta_json
+
+# Naming compatible with ISA-Tab
+print mm.meta_isa
 
 
 #====================================================
 # Get meta information for all mzML files
 #====================================================
-# from mzml_2_isa import mzml
-# import glob
-#
-# # get a list of mzml files
-# mzml_path = os.path.join(in_dir, "*.mzML")
-# mzml_files = [mzML for mzML in glob.glob(mzml_path)]
-#
-# # get meta information for all files
-# metalist = [ mzml.mzMLmeta(i).meta_json for i in mzml_files ]
-#
-# # print the meta information
-# for m in metalist:
-#     print m
+from mzml_2_isa import mzml
+import glob
+
+# get a list of mzml files
+mzml_path = os.path.join(in_dir, "*.mzML")
+mzml_files = [mzML for mzML in glob.glob(mzml_path)]
+
+# get meta information for all files
+metalist = [ mzml.mzMLmeta(i).meta_json for i in mzml_files ]
+
+# print the meta information
+for m in metalist:
+    print m

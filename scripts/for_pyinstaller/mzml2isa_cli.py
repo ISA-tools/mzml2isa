ecd18cb9a8c4a8e993f96fee7334ba2485aafb04
Thomas N Lawson <thomas.nigel.lawson@gmail.com>
2016-11-25 21:23:05 +0000

    update imports so that pyinstaller still has the reference

--- scripts/for_pyinstaller/mzml2isa_cli.py
+++ scripts/for_pyinstaller/mzml2isa_cli.py
@@ -1,4 +1,12 @@
-from mzml2isa import parsing
+from mzml2isa import parsing, mzml, isa
+import pronto
+
 
 if __name__ == '__main__':
+    
     parsing.main()
+
+    # this tmp list is so for automated checking
+    # of code sees that we are using the imports
+    # really they are just required for pyinstaller
+    tmp = [mzml, isa, pronto]

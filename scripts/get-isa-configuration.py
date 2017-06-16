d8831e78ee881276573bc921a01a36aa372ec3e2
Martin Larralde <martin.larralde@ens-cachan.fr>
2017-06-16 10:15:55 +0200

    Set up Travis to use new tests

--- scripts/get-isa-configuration.py
+++ scripts/get-isa-configuration.py
@@ -7,6 +7,7 @@ import os
 import sys
 import argparse
 import glob
+import six
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-o", "--output", default=os.curdir, action="store")
@@ -19,7 +20,7 @@ if not glob.glob(os.path.join(args.output, "*.xml")) or args.force:
 
     if not os.path.exists(args.output):
         os.mkdir(args.output)
-    
+
     ebi_ftp = ftplib.FTP("ftp.ebi.ac.uk")
     ebi_ftp.login()
 
@@ -30,11 +31,10 @@ if not glob.glob(os.path.join(args.output, "*.xml")) or args.force:
 
     zip_in_memory = zipfile.ZipFile(zip_in_memory)
 
-    configuration_files = [ n for n in zip_in_memory.namelist() 
+    configuration_files = [ n for n in zip_in_memory.namelist()
                               if n.startswith("Configurations/MetaboLights")]
 
     for configuration_file in configuration_files[1:]:
         with open(os.path.join(args.output, os.path.basename(configuration_file)), 'wb') as out_file:
-            with zip_in_memory.open(configuration_file, 'rU') as in_file:
+            with zip_in_memory.open(configuration_file, 'r' if six.PY3 else 'rU') as in_file:
                 out_file.write(in_file.read())
-

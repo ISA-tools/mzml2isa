ce4e02d632ac060217bafb48bbd92e3d23e52367
Martin Larralde <martin.larralde@ens-cachan.fr>
2018-08-03 14:50:23 +0200

    Restyle `mzml.py` and `imzml.py` code using `black`

--- mzml2isa/imzml.py
+++ mzml2isa/imzml.py
@@ -22,38 +22,157 @@ class ImzMLFile(MzMLFile):
     )
 
     _VOCABULARY = pronto.Ontology(
-        pkg_resources.resource_stream("mzml2isa", "ontologies/imagingMS.obo"), import_depth=1,
+        pkg_resources.resource_stream("mzml2isa", "ontologies/imagingMS.obo"), import_depth=1
     )
 
     @classmethod
-    def _generic_parameters(cls):
-        terms = super(ImzMLFile, cls)._generic_parameters()
+    def _assay_parameters(cls):
+        terms = super(ImzMLFile, cls)._assay_parameters()
 
-        terms['file_content'] = [
-                # _CVParameter(accession='MS:1000525', cv=True, name='Spectrum representation', plus1=True, value=False, software=False),
-                _CVParameter(accession='IMS:1000008', cv=True, name='Universally unique identifier', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1000009', cv=True, name='Binary file checksum type', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1000003', cv=True, name='Ibd binary type', plus1=False, value=False, software=False),
+        terms["file_content"] = [
+            # _CVParameter(accession='MS:1000525', cv=True, name='Spectrum representation', plus1=True, value=False, software=False),
+            _CVParameter(
+                accession="IMS:1000008",
+                cv=True,
+                name="Universally unique identifier",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000009",
+                cv=True,
+                name="Binary file checksum type",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000003",
+                cv=True,
+                name="Ibd binary type",
+                plus1=False,
+                value=False,
+                software=False,
+            ),
         ]
 
-        terms['scan_settings'] = [
-                _CVParameter(accession='IMS:1000040', cv=True, name='Linescan sequence', plus1=False, value=False, software=False),
-                _CVParameter(accession='IMS:1000041', cv=True, name='Scan pattern', plus1=False, value=False, software=False),
-                _CVParameter(accession='IMS:1000042', cv=True, name='Max count of pixel x', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1000043', cv=True, name='Max count of pixel y', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1000044', cv=True, name='Max dimension x', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1000045', cv=True, name='Max dimension y', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1000046', cv=True, name='Pixel size x', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1000047', cv=True, name='Pixel size y', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1000048', cv=True, name='Scan type', plus1=False, value=False, software=False),
-                _CVParameter(accession='IMS:1000049', cv=True, name='Line scan direction', plus1=False, value=False, software=False),
+        terms["scan_settings"] = [
+            _CVParameter(
+                accession="IMS:1000040",
+                cv=True,
+                name="Linescan sequence",
+                plus1=False,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000041",
+                cv=True,
+                name="Scan pattern",
+                plus1=False,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000042",
+                cv=True,
+                name="Max count of pixel x",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000043",
+                cv=True,
+                name="Max count of pixel y",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000044",
+                cv=True,
+                name="Max dimension x",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000045",
+                cv=True,
+                name="Max dimension y",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000046",
+                cv=True,
+                name="Pixel size x",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000047",
+                cv=True,
+                name="Pixel size y",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000048",
+                cv=True,
+                name="Scan type",
+                plus1=False,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000049",
+                cv=True,
+                name="Line scan direction",
+                plus1=False,
+                value=False,
+                software=False,
+            ),
         ]
 
-        terms['source'] = [
-                _CVParameter(accession='IMS:1001213', cv=True, name='Solvent flowrate', plus1=False, value=True, software=False ),
-                _CVParameter(accession='IMS:1001211', cv=True, name='Solvent', plus1=False, value=True, software=False ),
-                _CVParameter(accession='IMS:1000202', cv=True, name='Target material', plus1=False, value=True, software=False),
-                _CVParameter(accession='IMS:1001212', cv=True, name='Spray voltage', plus1=False, value=True, software=False),
+        terms["source"] = [
+            _CVParameter(
+                accession="IMS:1001213",
+                cv=True,
+                name="Solvent flowrate",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1001211",
+                cv=True,
+                name="Solvent",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1000202",
+                cv=True,
+                name="Target material",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="IMS:1001212",
+                cv=True,
+                name="Spray voltage",
+                plus1=False,
+                value=True,
+                software=False,
+            ),
         ]
 
         return terms
--- mzml2isa/mzml.py
+++ mzml2isa/mzml.py
@@ -80,20 +80,20 @@ class MzMLFile(object):
         "scan_num": "{root}/s:run/{spectrum}List",
         "cv": "{root}/s:cvList/s:cv",
         "raw_file": "{root}/s:fileDescription/s:sourceFileList/s:sourceFile",
-        'scan_sp': 's:cvParam',
-        'scan_combination': '{scanList}/s:cvParam',
-        'scan_configuration': '{scanList}/s:scan/s:cvParam',
-        'scan_binary': 's:binaryDataArrayList/s:binaryDataArray/s:cvParam',
-        'scan_activation': 's:precursorList/s:precursor/s:activation/s:cvParam',
-        'scan_isolation_window': 's:precursorList/s:precursor/s:isolationWindow/s:cvParam',
-        'scan_selected_ion': 's:precursorList/s:precursor/s:selectedIonList/s:selectedIon/s:cvParam',
-        'ref_sp': 's:referenceableParamGroupRef',
-        'ref_combination': '{scanList}/s:scan/s:referenceableParamGroupRef',
-        'ref_binary': '{scanList}/s:scan/s:referenceableParamGroupRef',
+        "scan_sp": "s:cvParam",
+        "scan_combination": "{scanList}/s:cvParam",
+        "scan_configuration": "{scanList}/s:scan/s:cvParam",
+        "scan_binary": "s:binaryDataArrayList/s:binaryDataArray/s:cvParam",
+        "scan_activation": "s:precursorList/s:precursor/s:activation/s:cvParam",
+        "scan_isolation_window": "s:precursorList/s:precursor/s:isolationWindow/s:cvParam",
+        "scan_selected_ion": "s:precursorList/s:precursor/s:selectedIonList/s:selectedIon/s:cvParam",
+        "ref_sp": "s:referenceableParamGroupRef",
+        "ref_combination": "{scanList}/s:scan/s:referenceableParamGroupRef",
+        "ref_binary": "{scanList}/s:scan/s:referenceableParamGroupRef",
     }
 
     _VOCABULARY = pronto.Ontology(
-        pkg_resources.resource_stream("mzml2isa", "ontologies/psi-ms.obo"), imports=False,
+        pkg_resources.resource_stream("mzml2isa", "ontologies/psi-ms.obo"), imports=False
     )
 
     def __init__(self, filesystem, path, vocabulary=None):
@@ -174,7 +174,7 @@ class MzMLFile(object):
 
     # NB:
     @classmethod
-    def _generic_parameters(cls):
+    def _assay_parameters(cls):
         terms = collections.OrderedDict()
 
         terms["file_content"] = [
@@ -364,55 +364,265 @@ class MzMLFile(object):
     def _scan_parameters(cls):
         terms = collections.OrderedDict()
 
-        terms['scan_sp'] = terms['ref_sp'] = {
-            _CVParameter(accession='MS:1000524', cv=True, name='Data file content', plus1=True, value=False, software=False),
-            _CVParameter(accession='MS:1000796', cv=False, name='Spectrum title', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000465', cv=True, name='Polarity', plus1=True, value=False, software=False),
-            _CVParameter(accession='MS:1000511', cv=False, name='MS Level', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000525', cv=True, name='Spectrum representation', plus1=True, value=False, software=False),
-            _CVParameter(accession='MS:1000504', cv=False, name='Base Peak m/z', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000505', cv=False, name='Base Peak intensity', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000285', cv=False, name='Total ion current', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000927', cv=False, name='Ion injection time', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000512', cv=False, name='Filter string', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000528', cv=False, name='Lowest observed m/z', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000527', cv=False, name='Highest observed m/z', plus1=True, value=True, software=False),
+        terms["scan_sp"] = terms["ref_sp"] = {
+            _CVParameter(
+                accession="MS:1000524",
+                cv=True,
+                name="Data file content",
+                plus1=True,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000796",
+                cv=False,
+                name="Spectrum title",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000465",
+                cv=True,
+                name="Polarity",
+                plus1=True,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000511",
+                cv=False,
+                name="MS Level",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000525",
+                cv=True,
+                name="Spectrum representation",
+                plus1=True,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000504",
+                cv=False,
+                name="Base Peak m/z",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000505",
+                cv=False,
+                name="Base Peak intensity",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000285",
+                cv=False,
+                name="Total ion current",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000927",
+                cv=False,
+                name="Ion injection time",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000512",
+                cv=False,
+                name="Filter string",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000528",
+                cv=False,
+                name="Lowest observed m/z",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000527",
+                cv=False,
+                name="Highest observed m/z",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
         }
 
-        terms['scan_combination'] = terms['ref_combination'] = {
-            _CVParameter(accession='MS:1000570', cv=True, name='Spectrum combination', plus1=True, value=False, software=False)
+        terms["scan_combination"] = terms["ref_combination"] = {
+            _CVParameter(
+                accession="MS:1000570",
+                cv=True,
+                name="Spectrum combination",
+                plus1=True,
+                value=False,
+                software=False,
+            )
         }
 
-        terms['scan_configuration'] = {
-            _CVParameter(accession='MS:1000016', cv=False, name='Scan start time', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000512', cv=False, name='Filter string', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000616', cv=False, name='Preset scan configuration', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000927', cv=False, name='Ion injection time', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000018', cv=True, name='Scan direction', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000019', cv=True, name='Scan law', plus1=True, value=True, software=False),
+        terms["scan_configuration"] = {
+            _CVParameter(
+                accession="MS:1000016",
+                cv=False,
+                name="Scan start time",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000512",
+                cv=False,
+                name="Filter string",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000616",
+                cv=False,
+                name="Preset scan configuration",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000927",
+                cv=False,
+                name="Ion injection time",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000018",
+                cv=True,
+                name="Scan direction",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000019",
+                cv=True,
+                name="Scan law",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
         }
 
-        terms['scan_isolation_window'] = {
-            _CVParameter(accession='MS:1000827', cv=False, name='Isolation window target m/z', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000828', cv=False, name='Isolation window lower offset', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000829', cv=False, name='Isolation window higher offset', plus1=True, value=True, software=False),
+        terms["scan_isolation_window"] = {
+            _CVParameter(
+                accession="MS:1000827",
+                cv=False,
+                name="Isolation window target m/z",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000828",
+                cv=False,
+                name="Isolation window lower offset",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000829",
+                cv=False,
+                name="Isolation window higher offset",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
         }
 
-        terms['scan_selected_ion'] = {
-            _CVParameter(accession='MS:1000744', cv=False, name='Selected ion m/z', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000041', cv=False, name='Charge state', plus1=True, value=True, software=False),
-            _CVParameter(accession='MS:1000042', cv=False, name='Peak intensity', plus1=True, value=True, software=False),
+        terms["scan_selected_ion"] = {
+            _CVParameter(
+                accession="MS:1000744",
+                cv=False,
+                name="Selected ion m/z",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000041",
+                cv=False,
+                name="Charge state",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000042",
+                cv=False,
+                name="Peak intensity",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
         }
 
-        terms['scan_activation'] = {
-            _CVParameter(accession='MS:1000044', cv=True, name='Dissociation method', plus1=True, value=False, software=False),
-            _CVParameter(accession='MS:1000045', cv=False, name='Collision Energy', plus1=True, value=True, software=False),
+        terms["scan_activation"] = {
+            _CVParameter(
+                accession="MS:1000044",
+                cv=True,
+                name="Dissociation method",
+                plus1=True,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000045",
+                cv=False,
+                name="Collision Energy",
+                plus1=True,
+                value=True,
+                software=False,
+            ),
         }
 
-        terms['scan_binary'] = terms['ref_binary'] = {
-            _CVParameter(accession='MS:1000518', cv=True, name='Binary data type', plus1=True, value=False, software=False),
-            _CVParameter(accession='MS:1000572', cv=True, name='Binary data compression type', plus1=True, value=False, software=False),
-            _CVParameter(accession='MS:1000513', cv=True, name='Binary data array', plus1=True, value=False, software=False),
+        terms["scan_binary"] = terms["ref_binary"] = {
+            _CVParameter(
+                accession="MS:1000518",
+                cv=True,
+                name="Binary data type",
+                plus1=True,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000572",
+                cv=True,
+                name="Binary data compression type",
+                plus1=True,
+                value=False,
+                software=False,
+            ),
+            _CVParameter(
+                accession="MS:1000513",
+                cv=True,
+                name="Binary data array",
+                plus1=True,
+                value=False,
+                software=False,
+            ),
         }
 
         return terms
@@ -422,25 +632,25 @@ class MzMLFile(object):
     @classmethod
     def _urlize_meta(cls, meta):
         for key, nested in six.iteritems(meta):
-            if 'accession' in nested:
-                nested['accession'] = cls._urlize_accession(nested['accession'])
-            if 'accession' in nested.get('unit', {}):
-                nested['unit']['accession'] = cls._urlize_accession(nested['unit']['accession'])
-            for entry in nested.get('entry_list', []):
-                if 'accession' not in entry and 'unit' not in entry:
+            if "accession" in nested:
+                nested["accession"] = cls._urlize_accession(nested["accession"])
+            if "accession" in nested.get("unit", {}):
+                nested["unit"]["accession"] = cls._urlize_accession(nested["unit"]["accession"])
+            for entry in nested.get("entry_list", []):
+                if "accession" not in entry and "unit" not in entry:
                     break
-                if 'accession' in entry:
-                    entry['accession'] = cls._urlize_accession(entry['accession'])
-                if 'accession' in entry.get('unit', {}):
-                    entry['unit']['accession'] = cls._urlize_accession(entry['unit']['accession'])
+                if "accession" in entry:
+                    entry["accession"] = cls._urlize_accession(entry["accession"])
+                if "accession" in entry.get("unit", {}):
+                    entry["unit"]["accession"] = cls._urlize_accession(entry["unit"]["accession"])
 
     @classmethod
     def _urlize_accession(cls, accession):
         try:
-            namespace, id_ = accession.split(':', 1)
-            if namespace in {'MS', 'UO'}:
+            namespace, id_ = accession.split(":", 1)
+            if namespace in {"MS", "UO"}:
                 url = "http://purl.obolibrary.org/obo/{}_{}"
-            elif namespace == 'IMS':
+            elif namespace == "IMS":
                 url = "http://www.maldi-msi.org/download/imzml/imagingMS.obo#{}:{}"
             else:
                 url = "{}:{}"
@@ -590,8 +800,8 @@ class MzMLFile(object):
                         ]
                     self._extract_software(soft_ref, param_info.name, meta)
 
-    def _extract_generic_parameters(self, meta):
-        terms = self._generic_parameters()
+    def _extract_assay_parameters(self, meta):
+        terms = self._assay_parameters()
         for location, term in six.iteritems(terms):
             for element in self._find_xpath(self._XPATHS[location]):
                 self._extract_cv_params(element, terms[location], meta)
@@ -769,7 +979,7 @@ class MzMLFile(object):
             meta["Instrument manufacturer"] = {
                 "accession": man.id,
                 "name": man.name,
-                "ref": man.id.split(':')[0],
+                "ref": man.id.split(":")[0],
             }
 
         try:  # Get associated software
@@ -793,24 +1003,24 @@ class MzMLFile(object):
         scan_num = next(self._find_xpath(self._XPATHS["scan_num"]))
         meta["Number of scans"] = {"value": int(scan_num.attrib["count"])}
 
-    def _collect_scan_info(self, meta):
+    def _extract_scan_parameters(self, meta):
         terms = self._scan_parameters()
 
-        for spectrum in self._find_xpath(self._XPATHS['sp']):
+        for spectrum in self._find_xpath(self._XPATHS["sp"]):
             for location, parameters in six.iteritems(terms):
                 xpath = self._XPATHS[location].format(**self.environment)
                 # we are extracting from a referenced parameter group
                 # so we must retrieve them before being able to extract
                 # the CV parameters
-                if location.startswith('ref'):
+                if location.startswith("ref"):
                     params = (
-                        self._referenceable_parameters[ref.attrib['ref']]
+                        self._referenceable_parameters[ref.attrib["ref"]]
                         for ref in spectrum.iterfind(xpath, self.namespace)
                     )
                     elements = (
                         element
                         for param in params
-                        for element in param.iterfind('s:cvParam', self.namespace)
+                        for element in param.iterfind("s:cvParam", self.namespace)
                     )
                 # we can extract the CV parameters directly
                 else:
@@ -833,7 +1043,7 @@ class MzMLFile(object):
     def metadata(self):
         meta = {}
 
-        self._extract_generic_parameters(meta)
+        self._extract_assay_parameters(meta)
         self._extract_instrument(meta)
         self._extract_derived_file(meta)
         self._extract_raw_file(meta)

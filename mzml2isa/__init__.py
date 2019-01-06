4b5d4591d577acc9f83884a3f6d0cc41c5c748d4
Martin Larralde <martin.larralde@ens-cachan.fr>
2019-01-06 16:23:08 +0100

    Release v1.0.1

--- mzml2isa/__init__.py
+++ mzml2isa/__init__.py
@@ -35,7 +35,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Thomas Lawson, Martin Larralde'
 __credits__ = 'Thomas Lawson, Martin Larralde, Ralf Weber, Reza Salek, Ken Haug, Christoph Steinbeck'
-__version__ = '1.0.0'
+__version__ = '1.0.1'
 __license__ = 'GPLv3'
 
 try:
